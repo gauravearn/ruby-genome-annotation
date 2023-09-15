402ef174804e3783ea35665b8bb9d59b4c898a6f
Gaurav Sablok <sablokg@gmail.com>
2023-09-15 15:45:54 +0530

    Update ruby_genome_annotation.rb

--- ruby_genome_annotation.rb
+++ ruby_genome_annotation.rb
@@ -4,8 +4,8 @@ def rubyAnnotationparse
   #the iterators at the faster rate. if you have dozens of
   #genome sequenced, simply mention the column number and the iterator
   #will hash the length
-  start_split = `for j in $(cat Athaliana_447_Araport11.gene.gff3 | cut -f 4 | grep "^[0-9]"); do echo $j; done | head -n 10`.split.map{|each| each.to_i}
-  end_split = `for j in $(cat Athaliana_447_Araport11.gene.gff3 | cut -f 5 | grep "^[0-9]"); do echo $j; done | head -n 10`.split.map{|each| each.to_i}
+  start_split = `for j in $(cat sample_file.txt | cut -f 4 | grep "^[0-9]"); do echo $j; done | head -n 10`.split.map{|each| each.to_i}
+  end_split = `for j in $(cat sample_file.txt | cut -f 5 | grep "^[0-9]"); do echo $j; done | head -n 10`.split.map{|each| each.to_i}
   for i in 0..start_split.length
       for j in 0..end_split.length
           puts end_split[j] - start_split[i]
